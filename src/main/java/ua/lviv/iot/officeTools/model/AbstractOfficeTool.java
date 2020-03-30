d4d56368531cdf3578820bd8f9106c3ca6675a97
YrSk-tech <32609324+YrSk-tech@users.noreply.github.com>
2020-03-30 06:58:07 +0300

    upload 4 lab

---
+++ src/main/java/ua/lviv/iot/officeTools/model/AbstractOfficeTool.java
@@ -0,0 +1,71 @@
+package ua.lviv.iot.officeTools.model;
+
+public abstract class AbstractOfficeTool {
+	private String producer;
+
+	private double priceInUAH;
+	private Brand brand;
+	private Colour colour;
+	private int weightInGrams;
+	private int warrantyInDays;
+	
+	public AbstractOfficeTool(String producer, double priceInUAH, Brand brand, Colour colour, int weightInGrams,
+			int warrantyInDays) {
+		super();
+		this.producer = producer;
+		this.priceInUAH = priceInUAH;
+		this.brand = brand;
+		this.colour = colour;
+		this.weightInGrams = weightInGrams;
+		this.warrantyInDays = warrantyInDays;
+	}
+	
+
+	public String getProducer() {
+		return producer;
+	}
+
+	public void setProducer(String producer) {
+		this.producer = producer;
+	}
+
+	public double getPriceInUAH() {
+		return priceInUAH;
+	}
+
+	public void setPriceInUAH(double priceInUAH) {
+		this.priceInUAH = priceInUAH;
+	}
+
+	public Brand getBrand() {
+		return brand;
+	}
+
+	public void setBrand(Brand brand) {
+		this.brand = brand;
+	}
+
+	public Colour getColour() {
+		return colour;
+	}
+
+	public void setColour(Colour colour) {
+		this.colour = colour;
+	}
+
+	public int getWeightInGrams() {
+		return weightInGrams;
+	}
+
+	public void setWeightInGrams(int weightInGrams) {
+		this.weightInGrams = weightInGrams;
+	}
+
+	public int getWarrantyInDays() {
+		return warrantyInDays;
+	}
+
+	public void setWarrantyInDays(int warrantyInDays) {
+		this.warrantyInDays = warrantyInDays;
+	}
+}
---
+++ src/test/java/ua/lviv/iot/officeTools/manager/BaseOfficeToolsManagerTest.java
@@ -0,0 +1,25 @@
+package ua.lviv.iot.officeTools.manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import ua.lviv.iot.officeTools.model.AbstractOfficeTool;
+import ua.lviv.iot.officeTools.model.Brand;
+import ua.lviv.iot.officeTools.model.Colour;
+import ua.lviv.iot.officeTools.model.Ruler;
+
+public abstract class BaseOfficeToolsManagerTest {
+
+	protected List<AbstractOfficeTool> ruler;
+
+	@BeforeEach
+	public void createRulers() {
+		ruler = new ArrayList<>();
+		ruler.add(new Ruler("Ukraine", 130, Brand.LEVENHUK, Colour.BLACK, 55, 61, 50));
+		ruler.add(new Ruler("Poland", 80, Brand.BUROMAX, Colour.WHITE, 120, 31, 30));
+		ruler.add(new Ruler("China", 150, Brand.REXEL_JOY, Colour.GREEN, 200, 0, 100));
+		ruler.add(new Ruler("Germany", 110, Brand.AXENT, Colour.BLUE, 150, 30, 60));
+	}
+}
